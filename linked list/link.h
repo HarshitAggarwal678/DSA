5fd68cb0d1be31f64f49e5936557322053c73021
Harsh <aggarwalh678@gmail.com>
2022-11-28 22:51:21 +0530

    header file of linked list basic functions

---
+++ linked list/link.h
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include<stdlib.h>
+struct node
+{
+int info;
+struct node *Next;
+}; 
+/***********************/
+
+/********************/
+struct node* GetNode()
+{
+  struct node *p;
+  p=(struct node*)malloc(sizeof(struct node));
+  return p;
+}
+/*******************/
+void InsBeg(struct node **START,int x)
+{
+  struct node *p;
+  p=GetNode();
+    p->info=x;
+  p->Next=*START;
+  *START=p;
+}
+/****************/
+void InsEnd(struct node **START,int x)
+{
+  struct node *q,*p;
+  q=*START;
+  while(q->Next!=NULL)
+    q=q->Next;
+  p=GetNode();
+  p->info=x;
+  p->Next=NULL;
+  q->Next=p;
+}
+/*****************/
+void Traverse(struct node **START)
+{
+  struct node *p;
+  p=*START;
+  while(p!=NULL)
+    {
+      printf("%d\t",p->info);
+      p=p->Next;
+    }
+}
