9f4cc426deeab5ed608e55b74995d6938ad525b6
Harsh <aggarwalh678@gmail.com>
2022-12-03 12:55:28 +0530

    header file of primitive functions for linear linear linked list

--- linked list/link.h
+++ linked list/link.h
@@ -5,16 +5,25 @@ struct node
 int info;
 struct node *Next;
 }; 
-/***********************/
-
-/********************/
+/*********/
+ int DelAft(struct node **p)
+ {
+ int x;
+ struct node *q;
+ q=(*p)->Next;
+ (*p)->Next=q->Next;
+ x=q->info;
+ free(q);
+ return x;
+ }
+/********/
 struct node* GetNode()
 {
   struct node *p;
   p=(struct node*)malloc(sizeof(struct node));
   return p;
 }
-/*******************/
+/*******/
 void InsBeg(struct node **START,int x)
 {
   struct node *p;
@@ -23,19 +32,37 @@ void InsBeg(struct node **START,int x)
   p->Next=*START;
   *START=p;
 }
-/****************/
+/******/
+int DelBeg(struct node **START)
+ {
+ struct node *p;
+ int x;
+ p=(*START);
+ (*START)=(*START)->Next;
+ x=p->info;
+ free(p);
+ return x;
+ }
+ /******/
 void InsEnd(struct node **START,int x)
 {
   struct node *q,*p;
   q=*START;
-  while(q->Next!=NULL)
+  if(q==NULL)
+  {
+    InsBeg(START,x);
+  }
+  else
+  {
+    while(q->Next!=NULL)
     q=q->Next;
-  p=GetNode();
-  p->info=x;
-  p->Next=NULL;
-  q->Next=p;
+    p=GetNode();
+    p->info=x;
+    p->Next=NULL;
+    q->Next=p;
+    }
 }
-/*****************/
+/*******/
 void Traverse(struct node **START)
 {
   struct node *p;
@@ -45,4 +72,23 @@ void Traverse(struct node **START)
       printf("%d\t",p->info);
       p=p->Next;
     }
+}
+struct node *reverse(struct node **start)
+{
+struct node *p,*n,*c;
+c=*start;
+p=NULL;
+n=c->Next;
+while (c!=NULL)
+{
+    c->Next=p;
+    p=c;
+    c=n;
+    if (n!=NULL)
+    {
+        n=n->Next;
+    }
+}
+*start=p;
+return *start;
 }
