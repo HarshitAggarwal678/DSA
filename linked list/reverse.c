53f2394f60334bafa1931eee5897ddf1cf67eb9f
Harsh <aggarwalh678@gmail.com>
2022-12-03 12:48:24 +0530

    reverse using stack

--- linked list/reverse.c
+++ linked list/reverse.c
@@ -1,4 +1,3 @@
-#include"link.h"
 #include<stdio.h>
 #include<stdlib.h>
 #define STACKSIZE 10
@@ -52,6 +51,79 @@ int Stacktop(struct stack *ptr)
     {
         x=ptr->item[ptr->top];
         return x;
+    }
+}
+struct node
+{
+int info;
+struct node *Next;
+}; 
+/*********/
+ int DelAft(struct node **p)
+ {
+ int x;
+ struct node *q;
+ q=(*p)->Next;
+ (*p)->Next=q->Next;
+ x=q->info;
+ free(q);
+ return x;
+ }
+/********/
+struct node* GetNode()
+{
+  struct node *p;
+  p=(struct node*)malloc(sizeof(struct node));
+  return p;
+}
+/*******/
+void InsBeg(struct node **START,int x)
+{
+  struct node *p;
+  p=GetNode();
+    p->info=x;
+  p->Next=*START;
+  *START=p;
+}
+/******/
+int DelBeg(struct node **START)
+ {
+ struct node *p;
+ int x;
+ p=(*START);
+ (*START)=(*START)->Next;
+ x=p->info;
+ free(p);
+ return x;
+ }
+ /******/
+void InsEnd(struct node **START,int x)
+{
+  struct node *q,*p;
+  q=*START;
+  if(q==NULL)
+  {
+    InsBeg(START,x);
+  }
+  else
+  {
+    while(q->Next!=NULL)
+    q=q->Next;
+    p=GetNode();
+    p->info=x;
+    p->Next=NULL;
+    q->Next=p;
+    }
+}
+/*******/
+void Traverse(struct node **START)
+{
+  struct node *p;
+  p=*START;
+  while(p!=NULL)
+    {
+      printf("%d\t",p->info);
+      p=p->Next;
     }
 }
 struct node *reverse(struct node **start,struct stack *s)
