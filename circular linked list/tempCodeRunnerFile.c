811fbd014bb714fe8a8c215eb733965f306090f8
Harshit Aggarwal <aggarwalh678@gmail.com>
2022-12-11 09:54:08 +0530

    circular linked list

---
+++ circular linked list/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+void fibo(int x)
+// {
+//     int *f;
+//     f=(int*)malloc(x*sizeof(int));
+//     *(f+0)=0;
+//     *(f+1)=1;
+//     int i=1;
+//     while(*(f+1)<x)
+//     {
+//         i++;
+//         *(f+i)=*(f+i-1)+*(f+i-2);
+//     }
+//     for(int j=0;j<i;j++)
+//     {
+//         printf("%d",*(f+j));
+//     }
+
+// }
