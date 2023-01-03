989f739b3c992ea3ce9ea001a203c8f5bf112b2c
harshit <aggarwalh678@gmail.com>
2023-01-04 00:36:42 +0530

    degree of vertex graph

---
+++ graph/degree.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int V,E;
+    cout<<"enter vertex";
+    cin>>V;
+    cout<<"enter edge";
+    cin>>E;
+    vector<int>ADl[V];
+    for(int i=1;i<E+1;i++)
+    {
+        cout<<"enter edges"<<i;
+        int a,b;
+        cin>>a>>b;
+        ADl[a].push_back(b);
+        ADl[b].push_back(a);
+    }
+    for(int i=0;i<V;i++)
+    {
+        cout<<i<<" "<<ADl[i].size()<<endl;
+    }
+return 0;
+}
